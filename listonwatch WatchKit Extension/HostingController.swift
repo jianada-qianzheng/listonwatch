ab77dd552ce307314c553db5e4434bd88ddc2d74
weizhi zhang <wagehk@gmail.com>
2019-07-06 13:03:02 -0400

    Initial Commit

---
+++ listonwatch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  listonwatch WatchKit Extension
+//
+//  Created by weizhi zhang on 2019-07-06.
+//  Copyright © 2019 weizhi zhang. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
